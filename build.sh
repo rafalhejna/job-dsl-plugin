6a3ceeda70a715950c114fd7cfa6573db1f6f2f2
Rafal Hejna <rafal.hejna@vonage.com>
2022-12-21 00:59:50 +0100

    Version extraction

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 ./gradlew build
+
+plugin_version=$(sed -n '1,1 s/version\=//' gradle.properties)
+
+echo "VERSION=${plugin_version}" >> $GITHUB_OUTPUT
--- publish.sh
+++ publish.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-plugin_version=$(sed -n '1,1 s/version\=//' gradle.properties)
+echo "Pushing job-dsl plugin version |$1| to Artifactory"
 
 curl -u "$ARTIFACTORY_USER":"$ARTIFACTORY_SECRET" \
-     -X PUT https://vonagecc.jfrog.io/artifactory/maven-jenkins-plugins/job-dsl-plugin/"$plugin_version"/job-dsl.hpi \
+     -X PUT https://vonagecc.jfrog.io/artifactory/maven-jenkins-plugins/job-dsl-plugin/"$1"/job-dsl.hpi \
      -T ./job-dsl.hpi
