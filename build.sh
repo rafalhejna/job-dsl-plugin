f269c8abfe8c7c72bc3f60ac767611b44af84d4d
Rafal Hejna <rafal.hejna@vonage.com>
2022-12-21 01:49:07 +0100

    Revert debug

--- build.sh
+++ build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-# ./gradlew build
+./gradlew build
 
 plugin_version=$(sed 's/version\=//' gradle.properties | head -n 1)
-sed 's/version\=//' gradle.properties
-echo '-------------------'
-sed 's/version\=//' gradle.properties | head -n 1
-echo '-------------------'
-sed -n '1,1 s/version\=//' gradle.properties
-echo '-------------------'
-cat gradle.properties
-echo '-------------------'
-echo "|$plugin_version|"
 
 echo "VERSION=${plugin_version}" >> $GITHUB_OUTPUT
