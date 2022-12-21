37b94890d6aa81bf7ab5d2c10e66d61d947e0dcd
Rafal Hejna <rafal.hejna@vonage.com>
2022-12-21 01:59:43 +0100

    debug

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-./gradlew build
+# ./gradlew build
 
 plugin_version=$(sed 's/version\=//' gradle.properties | head -n 1)
+
+echo "${plugin_version}"
 
 echo "VERSION=${plugin_version}" >> $GITHUB_OUTPUT
